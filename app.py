ca01d427f813bef45aad8e66455de02ac7c710f1
samruddhi <paisamruddhi4545@gmail.com>
2025-08-23 21:09:48 +0530

    updated python code

---
+++ app.py
@@ -0,0 +1,11 @@
+import base64
+import io
+from dotenv import load_dotenv #type:ignore
+load_dotenv()
+import streamlit as st
+import os 
+from PIL import Image
+import pdf2image
+import google.generativeai as genai #type :ignore
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
