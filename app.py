a37614b4675821a878b2caf311e14eac8ecaddc7
samruddhi <paisamruddhi4545@gmail.com>
2025-08-26 22:08:42 +0530

    modified

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ load_dotenv()
 import streamlit as st
 import os 
 from PIL import Image
-import pdf2image
 import google.generativeai as genai #type :ignore
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
