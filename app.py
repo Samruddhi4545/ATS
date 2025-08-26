1130899b8d98361436a22f5d6ee9816b4049bcc8
samruddhi <paisamruddhi4545@gmail.com>
2025-08-26 14:23:59 +0530

    modified

--- app.py
+++ app.py
@@ -9,3 +9,6 @@ import pdf2image
 import google.generativeai as genai #type :ignore
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_gemini_response(input,pdf_content,prompt): 
+    
