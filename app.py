48d3d9933da75b365eefded42012fc3ceaa24ccb
samruddhi <paisamruddhi4545@gmail.com>
2025-10-28 14:21:47 +0530

    app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import pdf2image #type: ignore
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 def get_gemini_response(input,pdf_content,prompt):
-    model=genai.GenerativeModel('gemini-1.0-pro-vision-latest')
+    model=genai.GenerativeModel('gemini-2.5-flash')
     response=model.generate_content([input,pdf_content[0],prompt])
     return response.text
 
